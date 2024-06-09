d36c7dbac7bbf2007ac4f9f83b4fa7584e2a2acc
MinhKhoiUET <minhkhoi281103@gmail.com>
2024-06-09 15:33:02 +0700

    complete header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const  App = () => {
     <>
 
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-          <Header></Header>
+          <Header/>
       </div>
 
        <ButtonGradient/>
